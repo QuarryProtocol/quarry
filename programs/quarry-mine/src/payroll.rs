4455b68e61edd9ca82a008096716feea7fad3c12
michaelhly <michaelhly@gmail.com>
2021-09-12 23:14:40 -0400

    Better sanity check test

--- programs/quarry-mine/src/payroll.rs
+++ programs/quarry-mine/src/payroll.rs
@@ -110,10 +110,13 @@ impl Payroll {
         let net_new_rewards = self
             .calculate_reward_per_token_unsafe(current_ts)?
             .checked_sub(rewards_per_token_paid)?;
-        (tokens_deposited as u128)
-            .checked_mul(net_new_rewards)?
-            .checked_div(PRECISION_MULTIPLIER)?
-            .checked_add(rewards_earned.into())
+        let rewards_earned = U192::from(tokens_deposited)
+            .checked_mul(net_new_rewards.into())?
+            .checked_div(PRECISION_MULTIPLIER.into())?
+            .checked_add(rewards_earned.into())?;
+
+        let precise_rewards_earned: u128 = rewards_earned.try_into().ok()?;
+        Some(precise_rewards_earned)
     }
 
     /// Calculates the amount of rewards earned for the given number of staked tokens, with safety checks.
@@ -153,7 +156,6 @@ impl Payroll {
         let quarry_rewards_accrued = U192::from(time_worked)
             .checked_mul(self.annual_rewards_rate.into())?
             .checked_div(SECONDS_PER_YEAR.into())?;
-
         let net_rewards_per_token = self
             .rewards_per_token_stored
             .checked_sub(rewards_per_token_paid)?;
@@ -204,6 +206,10 @@ impl Payroll {
 #[cfg(test)]
 mod tests {
     use crate::MAX_ANNUAL_REWARDS_RATE;
+    /// Maximum seconds elapsed between two checkpoints.
+    /// [i32::MAX] corresponds to about 70 years.
+    const MAX_SECONDS_BETWEEN_CHECKPOINTS: i64 = i32::MAX as i64;
+    const MAX_TOTAL_TOKENS: u64 = 1_000_000_000_000_000;
 
     use super::*;
     use num_traits::ToPrimitive;
@@ -235,11 +241,23 @@ mod tests {
     }
 
     prop_compose! {
-        pub fn part_and_total()(
+        pub fn part_and_total_small()(
             total in 0..u64::MAX
         )(
             // use a really small number here
             part in 0..cmp::min(100_000_u64, total),
+            total in Just(total)
+        ) -> (u64, u64) {
+           (part, total)
+       }
+    }
+
+    prop_compose! {
+        pub fn part_and_total()(
+            total in 0..MAX_TOTAL_TOKENS
+        )(
+            // use a really small number here
+            part in 0..total,
             total in Just(total)
         ) -> (u64, u64) {
            (part, total)
@@ -253,7 +271,7 @@ mod tests {
             num_updates in 1..100_i64,
             (final_ts, initial_ts) in total_and_intermediate_ts(),
             annual_rewards_rate in 0..=MAX_ANNUAL_REWARDS_RATE,
-            (my_tokens_deposited, total_tokens_deposited) in part_and_total()
+            (my_tokens_deposited, total_tokens_deposited) in part_and_total_small()
         ) {
             const EPSILON: f64 = 0.0001;
 
@@ -268,7 +286,6 @@ mod tests {
                     total_tokens_deposited
                 );
                 let current_ts = initial_ts + (((final_ts - initial_ts) as u128) * (i as u128) / (num_updates as u128)).to_i64().unwrap();
-                prop_assume!(current_ts <= i64::MAX >> 1);
                 rewards_per_token_stored = payroll.calculate_reward_per_token(current_ts).unwrap();
                 last_checkpoint_ts = current_ts;
             }
@@ -301,8 +318,8 @@ mod tests {
         #[test]
         fn test_sanity_check(
             annual_rewards_rate in 0..=MAX_ANNUAL_REWARDS_RATE,
-            rewards_already_earned in u64::MIN..u64::MAX,
-            (rewards_per_token_stored, rewards_per_token_paid) in total_and_intermediate_u64(),
+            rewards_already_earned in u64::MIN..MAX_TOTAL_TOKENS,
+            (rewards_per_token_paid, rewards_per_token_stored) in part_and_total(),
             (current_ts, last_checkpoint_ts) in total_and_intermediate_ts(),
             (my_tokens_deposited, total_tokens_deposited) in part_and_total()
         ) {
@@ -314,44 +331,45 @@ mod tests {
                 total_tokens_deposited
             );
 
-            let rewards_earned = payroll.calculate_rewards_earned(current_ts, my_tokens_deposited, rewards_per_token_paid.into(), rewards_already_earned).unwrap();
+            let amount_claimable_less_already_earned = payroll.calculate_rewards_earned(current_ts, my_tokens_deposited, rewards_per_token_paid.into(), rewards_already_earned).unwrap() - rewards_already_earned as u128;
             let upperbound = payroll.calculate_claimable_upper_bound_unsafe(current_ts, rewards_per_token_paid.into()).unwrap();
 
-            assert!(upperbound >= rewards_earned.into(), "rewards_earned: {}, upperbound: {}", rewards_earned, upperbound);
+            assert!(upperbound >= amount_claimable_less_already_earned.into(), "amount_claimable_less_already_earned: {}, upperbound: {}", amount_claimable_less_already_earned, upperbound);
         }
     }
 
-    #[test]
-    fn test_sanity_check_specific() {
-        let annual_rewards_rate = 365_000_000_000_000;
-        let total_tokens_deposited = 1_000_000;
+    // #[test]
+    // fn test_sanity_check_specific() {
+    //     let total_tokens_deposited = 1_000_000;
+    //     let annual_rewards_rate = 365_000_000_000_000;
+    //     let rewards_per_token_stored: u128 = 576247267536447296791024;
 
-        let rewards_per_token_stored: u128 = 576247267536447296791024;
+    //     let last_checkpoint_ts = 1631494525;
+    //     let payroll = Payroll::new(
+    //         i64::MAX,
+    //         last_checkpoint_ts,
+    //         annual_rewards_rate,
+    //         rewards_per_token_stored,
+    //         total_tokens_deposited,
+    //     );
 
-        let last_checkpoint_ts = 1631494525;
-        let payroll = Payroll::new(
-            i64::MAX,
-            last_checkpoint_ts,
-            annual_rewards_rate,
-            rewards_per_token_stored,
-            total_tokens_deposited,
-        );
+    //     let current_ts = 1631494531;
 
-        let current_ts = 1631494531;
-        let rewards_earned = payroll
-            .calculate_rewards_earned(current_ts, total_tokens_deposited, 0, 0)
-            .unwrap();
-        let upperbound = payroll
-            .calculate_claimable_upper_bound_unsafe(current_ts, 0)
-            .unwrap();
+    //     let rewards_earned = payroll
+    //         .calculate_rewards_earned(current_ts, total_tokens_deposited, 0, 0)
+    //         .unwrap();
+    //     let upperbound = payroll
+    //         .calculate_claimable_upper_bound_unsafe(current_ts, 0)
+    //         .unwrap();
 
-        assert!(
-            upperbound >= rewards_earned.into(),
-            "rewards_earned: {}, upperbound: {}",
-            rewards_earned,
-            upperbound
-        );
-    }
+
+    //     assert!(
+    //         upperbound >= rewards_earned.into(),
+    //         "rewards_earned: {}, upperbound: {}",
+    //         rewards_earned,
+    //         upperbound
+    //     );
+    // }
 
     proptest! {
         #[test]
@@ -402,18 +420,11 @@ mod tests {
     }
 
     prop_compose! {
-        pub fn total_and_intermediate_ts()(total in 0..i64::MAX)
-                        (intermediate in 0..total, total in Just(total))
-                        -> (i64, i64) {
-           (total, intermediate)
-       }
-    }
-
-    prop_compose! {
-        pub fn total_and_intermediate_u64()(total in 0..u64::MAX)
-                        (intermediate in 0..total, total in Just(total))
-                        -> (u64, u64) {
-           (total, intermediate)
+        pub fn total_and_intermediate_ts()(
+          elapsed_seconds in 0..MAX_SECONDS_BETWEEN_CHECKPOINTS,
+          last_checkpoint_ts in 0..(i64::MAX - MAX_SECONDS_BETWEEN_CHECKPOINTS),
+        ) -> (i64, i64) {
+          (last_checkpoint_ts + elapsed_seconds, last_checkpoint_ts)
        }
     }
 }
